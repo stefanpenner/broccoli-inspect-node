90774fb7cc91104b227188f01e0925b25c42c7f6
Stefan Penner <stefan.penner@gmail.com>
2017-04-10 19:59:22 -0700

    fixup

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 'use strict';
+
 const walkSync = require('walk-sync');
 
-module.exports = function inputs(plugin) {
-  return plugin._inputNodes.map(node => {
-    return {
-      name: node.toString(),
-      inputCount: node.inputPaths.length,
-      fileCount: walkSync(node.ouputPath).length
-    };
-  });
-}
+module.exports = function(node) {
+  let name, outputPath;
+
+  if (typeof node === 'string') {
+    name = node;
+    outputPath = node;
+  } else {
+    name = node.toString();
+    outputPath = node.outputPath || node._directoryPath;
+  }
+
+  let fileCount = walkSync(outputPath).length;
+  let inputCount = (node.inputPaths || []).length;
+
+  return {
+    name,
+    inputCount,
+    fileCount
+  };
+};
+
+module.exports.ofPlugin = function(plugin) {
+  return plugin._inputNodes.map(module.exports);
+};
+
